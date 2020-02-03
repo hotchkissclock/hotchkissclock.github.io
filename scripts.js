2af5e0a29ec2bcfbb6f68ea13748ae259463b878
Kush Banker <kushbanker@gmail.com>
2020-02-02 20:10:55 -0500

    Clock counts down

--- scripts.js
+++ scripts.js
@@ -4,28 +4,35 @@ updateTime(clock, label);
 
 function updateTime(clock, label) {
   var date = new Date();
-  var hour = date.getHours();
-  // hour = (hour > 12) ? (hour - 12) : hour;
+  // var hour = date.getHours();
   var min = date.getMinutes();
   var sec = date.getSeconds();
 
-  var periods = [0, 830, 920, 1520];
+  /* Testing*/
+  var hour = 9;
+  // var min = 40;
+  // var sec = 2;
+
+  var periods = [830, 920, 1000, 1520];
 
   var time = (hour * 100) + min;
   var periodNum = 0;
 
-  for(i = 0; i < periods.length; i++)
+  for(i = 1; i < periods.length; i++)
   {
-    if (time > periods[i])
+    if (time > periods[i-1] && time <= periods[i])
     {
       periodNum = i;
       break;
     }
   }
       
+  var hoursToNextPeriod = Math.floor((periods[periodNum] - time)/ 100);
+  var minsToNextPeriod = (periods[periodNum] > time) ? (periods[periodNum] - time - 40): (time - periods[periodNum] - 40);
+  var secsToNextPeriod = 60 - sec;
 
-  clock.innerHTML = hour + " : " + min + " : " + sec;
-  label.innerHTML = "Period " + periodNum + " " + time;
+  clock.innerHTML = hoursToNextPeriod + " : " + minsToNextPeriod + " : " + secsToNextPeriod;
+  label.innerHTML = "Period " + periodNum; 
 
   var timer = setTimeout(function(){ updateTime(clock, label) }, 1000 );
 }
