6d500c55253cf825c58a5c9f37389b2e6eafa7f9
Kush Banker <kushbanker@gmail.com>
2020-02-14 16:13:43 -0500

    Changes structure to allow for names

--- scripts.js
+++ scripts.js
@@ -14,28 +14,33 @@ function updateTime(clock, label) {
   // var min = 40;
   // var sec = 2;
 
-  var periods = [830, 920, 1000, 1040, 1125, 1210, 1255, 1345, 1435, 1520];
+  var schedule = {
+    monday: [[830, 830,"Period 0"], [915, 920,"Period 1"], [955, 1000,"Period 2"], [1035, 1040,"Chapel/Class Meeting"], 
+      [1120, 1125,"Period 3"], [1205, 1210, "Period 4"], [1255, 1255,"Period 5a"], [1340, 1345, "Period 5b"], [1430, 1435, "Period 6"], [1520, 1520, "Period 7"]]
+  };
+
+  var periods = schedule.monday;
 
   var time = (hour * 100) + min;
   var periodNum = 0;
 
   for(i = 1; i < periods.length; i++)
   {
-    if (time > periods[i-1] && time <= periods[i])
+    if (time > periods[i-1][1] && time <= periods[i][0])
     {
       periodNum = i;
       break;
     }
   }
       
-  var hoursToNextPeriod = Math.floor((periods[periodNum] - time)/ 100);
-  var timeDiff = periods[periodNum] - time;
+  var hoursToNextPeriod = Math.floor((periods[periodNum][0] - time)/ 100);
+  var timeDiff = periods[periodNum][0] - time;
   var minsToNextPeriod = (timeDiff > 60) ? timeDiff - 40: timeDiff;
   var secsToNextPeriod = 60 - sec;
 
   clock.innerHTML = hoursToNextPeriod + " : " + minsToNextPeriod + " : " + secsToNextPeriod;
   // label.innerHTML = "Period " + periodNum + " " + time + " " + periods[periodNum];
-  label.innerHTML = "Period " + periodNum;
+  label.innerHTML = periods[periodNum][2];
 
   var timer = setTimeout(function(){ updateTime(clock, label) }, 1000 );
 }
